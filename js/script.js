25447ab4e240d4128b74b0af85bfe20afbf98bd3
Eric Polanco <ericpolancot@gmail.com>
2023-05-05 08:52:27 -0400

    pseudocode added to script.js

---
+++ js/script.js
@@ -0,0 +1,31 @@
+/*----- constants -----*/
+    //computed pattern
+
+    //user's pattern
+
+/*----- state variables -----*/
+    //turn
+
+
+/*----- cached elements  -----*/
+    //screen elements
+
+
+
+/*----- event listeners -----*/
+    //game controls [ start ]
+
+    //user click pattern 
+
+
+/*----- functions -----*/
+    //init()
+
+    //create random pattern
+
+    //compare user's pattern with computer's pattern
+
+    //keep track of score
+
+    //render()
+
